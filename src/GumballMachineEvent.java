0115ebccc7876a19369235d5b8f323c6b48dacc1
Supakrit Pamakama <spk.zxrtxn@gmail.com>
2024-07-14 16:26:07 +0700

    add GumballMachineEvent interface

---
+++ src/GumballMachineEvent.java
@@ -0,0 +1,13 @@
+/*
+ * Name:    Supakrit Pamakama
+ * ID:      6510450968
+ */
+
+public interface GumballMachineEvent {
+    public void insertQuarter();
+    public void ejectQuarter();
+
+    public void turnCrank();
+
+    public void dispenseGumball();
+}
